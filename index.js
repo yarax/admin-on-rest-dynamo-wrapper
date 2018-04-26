81eff3a86be347a8804aa681345f48bc1d28ff2d
Roman Krivtsov <Roman.Krivtsov@cocomore.com>
2018-04-26 10:08:23 +0200

    stable pagination, filter

--- index.js
+++ index.js
@@ -5,19 +5,44 @@
 
 function listWrapper(req/*: Request*/, res/*: Response*/) {
   return (Model/*: any*/)/*: Promise<{}>*/ => {
+    //code.options.description
+    let searchField = null;
+    if (Model['$__'] && typeof(Model['$__'].table.schema.attributes) === 'object') {
+      const attrs = Model['$__'].table.schema.attributes;
+      Object.keys(attrs).forEach((attr) => {
+        if (attrs[attr].options && typeof (attrs[attr].options.description) === 'string') {
+          try {
+            const conf = JSON.parse(attrs[attr].options.description);
+            if (conf.searchField) searchField = attr;
+          } catch(e) {
+            console.log('Cannot parse field description', attrs[attr].options.description, ' of attr ', attr);
+          }
+        }
+      });
+    }
     const cursor = Model.scan();
     const lastKey = req.get('x-lastkey');
     const limit = req.query.per_page;
-    if (limit) {
-      cursor.limit(limit);
+    const filter = req.query.filter;
+
+    if (filter) {
+      if (!searchField) {
+        console.log('Cannot apply filter without searchField');
+      } else {
+        console.log('searchField', searchField, ' contains ', filter);
+        cursor.where(searchField).contains(filter);
+      }
     }
-    if (lastKey) {
+    if (lastKey && lastKey !== 'undefined') {
       try {
         const parsedLastKey = JSON.parse(lastKey);
         cursor.startAt(parsedLastKey);
       } catch (e) {
         console.log('Could not parse lastKey', lastKey, e);
       }
+    }
+    if (limit && !filter) {
+      cursor.limit(limit);
     }
     return cursor;
   }
@@ -26,7 +51,10 @@ function listWrapper(req/*: Request*/, res/*: Response*/) {
 function responseWrapper(res/*: Response*/) {
   return (items/*: DynamoList*/) => {
     res.set('x-lastkey', JSON.stringify(items.lastKey));
-    res.set('x-total-count', items.scannedCount);
+    res.set('x-total-count', 1000000);
+    res.set('Access-Control-Expose-Headers', 'x-total-count, x-lastkey');
+    res.set('Access-Control-Allow-Methods', '*');
+    res.set('Access-Control-Allow-Headers', '*');
     res.json(items);
     return items;
   }
